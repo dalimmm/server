eeaa470fd39e4a7215f5e6712578f7c437073aab
ONE <31034469+choco-one@users.noreply.github.com>
2024-04-02 21:54:17 +0900

    init: 초기 프로젝트 설정

---
+++ src/main/java/com/dalim/DalimApplication.java
@@ -0,0 +1,13 @@
+package com.dalim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalim/DalimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
