498f4bcabf8872320578b4a4a7e38e0501f62782
Go-Jaecheol <gojaech@naver.com>
2024-05-14 19:16:08 +0900

    feat: Todo 엔티티 추가

---
+++ src/main/java/com/dalim/todo/domain/Todo.java
@@ -0,0 +1,40 @@
+package com.dalim.todo.domain;
+
+import com.dalim.sprint.domain.Sprint;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sprint_id")
+    private Sprint sprint;
+
+    private String content;
+
+    private boolean done;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/dalim/todo/ui/TodoController.java
@@ -0,0 +1,8 @@
+package com.dalim.todo.ui;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+}
